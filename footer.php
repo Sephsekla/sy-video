5385aa255dbd9d090c115c6a700c94f4f74e1dac
Joe Bailey-Roberts <joe@Jons-MacBook-Pro.local>
2019-04-28 10:18:10 +0100

    Setup footer content

--- footer.php
+++ footer.php
@@ -15,17 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info container">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sy' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'sy' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'sy' ), 'sy', '<a href="https://joebr.io">Joe Bailey-Roberts</a>' );
-				?>
+			&copy; Shaun Young <?php echo date('Y') ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->

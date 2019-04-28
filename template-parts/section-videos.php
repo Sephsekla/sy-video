097b336574b2ae520194a5c84fbfd9215ae1b873
Joe Bailey-Roberts <joe@Jons-MacBook-Pro.local>
2019-04-28 15:35:14 +0100

    Setup video loop

--- template-parts/section-videos.php
+++ template-parts/section-videos.php
@@ -1,7 +1,27 @@
 <section class="padsection">
     <div class="container">
             <div class="row justify-content-center">
-                <div class="col-12 col-md-6 col-lg-3">VIDEO</div>
+               <?php
+
+                    $video_query = new WP_Query(array(
+                        'post_type' => 'video',
+                        'posts_per_page' => 1,
+                    ));
+
+                        if($video_query->have_posts()){
+                            while($video_query->have_posts()){
+                                $video_query->the_post();
+
+                                ?>
+
+                                <div class="col-12 col-md-6 col-lg-4">
+                                     <?php the_post_thumbnail("video") ?>
+                                </div>
+
+                <?php
+                            }
+                        }
+                ?>  
             </div>
     </div>
 </section>

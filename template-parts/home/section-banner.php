4214d3433b8c07622a3afddacee4f9dbe3ef003e
Joe Bailey-Roberts <joe@Jons-MacBook-Pro.local>
2019-07-07 19:40:26 +0100

    Add video banner

--- template-parts/home/section-banner.php
+++ template-parts/home/section-banner.php
@@ -1,3 +1,5 @@
 <section class="banner banner-home" style="background-image: url(<?php the_post_thumbnail_url( 'full' ); ?>)">
-
+<div class="embed-responsive embed-responsive-16by9">
+<iframe src="https://player.vimeo.com/video/76979871?background=1" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+</div>
 </section>

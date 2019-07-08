244ac633f79e93a4cb7266e94d305f3ccc784623
Joe Bailey-Roberts <joe@Jons-MacBook-Pro.local>
2019-07-08 19:09:51 +0100

    Add banner overlay

--- template-parts/home/section-banner.php
+++ template-parts/home/section-banner.php
@@ -2,4 +2,9 @@
 <div class="embed-responsive embed-responsive-16by9">
 <iframe src="https://player.vimeo.com/video/76979871?background=1" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 </div>
+<div class="overlay">
+    <div class="container">
+         <h1>Lorem Ipsum Dolor <span class="color_teal">Sit Amet</span></h1>
+    </div>
+</div>
 </section>

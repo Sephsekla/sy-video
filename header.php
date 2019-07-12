57f337b34af6978e9b320168b90bb2cc40afe1fd
Joe Bailey-Roberts <joe@Jons-MacBook-Pro.local>
2019-07-12 21:04:02 +0100

    Style button

--- header.php
+++ header.php
@@ -49,7 +49,13 @@
 			</div>
 			<div class="col-6 col-md-9">
 				<nav id="site-navigation" class="main-navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'sy' ); ?></button>
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+						
+						</button>
 					<?php
 					wp_nav_menu( array(
 						'theme_location' => 'menu-1',

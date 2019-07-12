504204f692357a6a4a54d76c34fd3465a9077915
Joe Bailey-Roberts <joe@Jons-MacBook-Pro.local>
2019-07-12 21:59:10 +0100

    Update package workflow

--- webpack.common.js
+++ webpack.common.js
@@ -3,10 +3,11 @@ const path = require('path');
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
+const autoprefixer = require('autoprefixer');
+
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const extractSass = new ExtractTextPlugin({
-    filename: "main.min.css"
-
+  filename: "main.min.css",
 });
 
 const extractAdmin = new ExtractTextPlugin({
@@ -18,16 +19,30 @@ const extractSettings = {
       loader: "css-loader",
       options: {
         url: false,
+        sourceMap: true
+
       }
-  }, 
-  
-  
-  
-  {
-    loader: "sass-loader",
-  },
- 
-],
+    },
+
+    {
+      loader: 'postcss-loader',
+      options: {
+        plugins: () => [autoprefixer({
+          'browsers': ['> 1%', 'last 2 versions']
+        })],
+        sourceMap: true
+      }
+    },
+
+    {
+      loader: "sass-loader",
+      options: {
+        sourceMap: true
+      }
+
+    },
+
+  ],
   // use style-loader in development
   fallback: "style-loader"
 }
@@ -40,53 +55,49 @@ module.exports = {
       assets: path.resolve(__dirname, '../dist/assets'),
     },
   },
-  
-  entry: 
-  {
-  
-    main: './src/index.js',
 
-  }
-  ,
+  entry: {
+
+    main: './src/index.js'
+
+  },
   plugins: [
-      extractSass,
-      extractAdmin
+    extractSass,
+    extractAdmin
   ],
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
-    },
-  
-    
+  },
+
+
 
   //When run in WordPress we want to use external jquery
   externals: {
     jquery: 'jQuery'
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-       use: [{
+        use: [{
           loader: 'babel-loader',
           options: {
             presets: ['babel-preset-env']
           }
-        },
-        { loader: "ifdef-loader", options: {
-          DEBUG: false,
-        } } 
-        ]
+        }, {
+          loader: "ifdef-loader",
+          options: {
+            DEBUG: false,
+          }
+        }]
       },
       {
         test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {}
-          }
-        ]
+        use: [{
+          loader: 'file-loader',
+          options: {}
+        }]
       },
 
       {
@@ -95,13 +106,13 @@ module.exports = {
         use: extractSass.extract(extractSettings)
       },
 
-     {
+      {
         test: /admin\.scss$/,
         use: extractAdmin.extract(extractSettings)
-      }, 
-      
-      
-      
+      },
+
+
+
       {
         test: /\.css$/,
         loader: 'style-loader',
@@ -113,7 +124,7 @@ module.exports = {
           minimize: true
         }
       }
-    
+
     ]
   }
 

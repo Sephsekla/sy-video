e965c37304051dba10b41841ad30d61c82938d04
Joe Bailey-Roberts <dev@joebr.io>
2019-08-29 21:24:27 +0100

    Update contact

--- template-parts/section-form.php
+++ template-parts/section-form.php
@@ -1,14 +1,19 @@
 <section class="form padsection bg_ow">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-lg-6"><?php  echo do_shortcode('[forminator_form id="25"]') ?></div>
+            <div class="col-12 col-lg-6"><?php
+            
+            echo forminator_form(25);
+            
+            ?></div>
             <div class="col-12 col-lg-6">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <?php
                 
-                 <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-
-                <p><strong>E</strong> example@example.com<br>
-                <strong>T</strong> 01234 567890
+                $contact = get_field('contact_methods');
+                
+                ?>
+                <p class="contact-methods"><strong>E</strong> <a href="mailto:<?php echo $contact['email'] ?>"><?php echo $contact['email'] ?></a><br>
+                <strong>T</strong> <a href=tel:<?php echo $contact['phone'] ?>"><?php echo $contact['phone'] ?></a>
                 </p>
             </div>
         </div>

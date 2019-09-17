e9c7d2ca5a25b2b7b0b433790e228ce157103aa9
Joe Bailey-Roberts <dev@joebr.io>
2019-09-17 08:19:00 +0100

    Setup video loop

--- template-parts/home/section-videos.php
+++ template-parts/home/section-videos.php
@@ -5,17 +5,21 @@
 
                     $video_query = new WP_Query(array(
                         'post_type' => 'video',
-                        'posts_per_page' => -1,
+                        'posts_per_page' => 6,
                     ));
 
                         if($video_query->have_posts()){
+                            $i = 0;
                             while($video_query->have_posts()){
                                 $video_query->the_post();
 
+                                echo (0 === $i%5) ? '<div class="col-12 col-md-6 video-col wide">' : '<div class="col-12 col-md-6 col-lg-3 video-col">'
+
+
                                 ?>
 
-                                <div class="col-12 col-md-6 col-lg-4">
-                                    <a href="<?php echo get_field('video',false,false)?>" class="lightbox-toggle video">
+                                
+                                    <a href="<?php echo get_field('video',false,false)?>" class="lightbox-toggle video" style="background-image: url(<?php echo get_the_post_thumbnail_url(0, 'video') ?>)">
                                         <?php the_post_thumbnail("video") ?>
                                         <div class="overlay">
                                             <div class="inner">
